1c31a486b0c01d8f9dc95d0de7667e1237248cac
Emma Coutu <emma@coutu.io>
2021-08-31 23:44:34 -0400

    feat: add link to instagram page

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div class="app">
     <Flyer flyer="../assets/flyer1.jpeg" />
     <Guidelines />
+    <InstagramIcon class="instagram-icon" user="reunition.party" />
   </div>
 </template>
 
 <script>
 import Flyer from './components/Flyer.vue';
 import Guidelines from './components/Guidelines.vue';
+import InstagramIcon from './components/InstagramIcon.vue';
 
 export default {
   name: 'App',
   components: {
     Flyer,
     Guidelines,
+    InstagramIcon,
   },
 };
 </script>
@@ -34,5 +37,11 @@ body {
   color: #2c3e50;
   width: 100%;
   height: 100%;
+}
+
+.instagram-icon {
+  width: 48px;
+  height: 48px;
+  stroke: white;
 }
 </style>
---
+++ src/components/InstagramIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <a href="https://instagram.com/re.unition" target="_blank">
+    <img alt="link to instagram" src="../assets/insta-color.png" />
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'InstagramIcon',
+  props: {
+    user: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 48px;
+  height: 48px;
+}
+</style>
